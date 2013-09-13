8aeaf842fb3977788f42b76272ba22d717cf849d
Angelo Tata <angelo.tata@gmail.com>
2013-09-13 07:41:12 -0400

    Initial commit

---
+++ AnobiiSelectors.js
@@ -0,0 +1,11 @@
+var AnobiiSelectors = {
+    BOOKS_URLS: '.editreview',
+    BOOKS_ISBNS: '.simple_list_view_container td.title a',
+    TITLE: '#bookTitle',
+    STARS: '.rate_book_1.selected',
+    CATEGORIES: '.categoryMy',
+    STATUS: 'select.progress option:selected',
+    READING_DATES: '.startDate dd a.datePickerText'
+};
+
+module.exports = AnobiiSelectors;
---
+++ README.md
@@ -0,0 +1,6 @@
+# Usage
+Username and password must be provided on the command line:
+
+```
+    --credentials.username=YOUR_ANOBII_USERNAME --credentials.password=YOUR_ANOBII_PASSWORD
+```
